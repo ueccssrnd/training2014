39d423ed2f851f611980b78af87adfb361a01ef9
Joey Hipolito <hi@joeyhipolito.com>
2014-01-18 01:35:51 +0800

    Start day2

---
+++ day2/examples/1.php
@@ -0,0 +1,5 @@
+<ul>
+<?php for($i=0; $i<3; $i++): ?>
+  <li><?php echo $i; ?></li>
+<?php endfor ?>
+</ul>
---
+++ day2/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'R&D Training 2014',
+    subtitle: 'The not so basic (Day 2)',
+    eventInfo: {
+     title: 'R&D Training',
+     date: '07/17/2014'
+    },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Joey Hipolito',
+    company: 'Web Dev\'t Division Head<br>UE CCSS R&D',
+    gplus: 'https://plus.google.com/116193099669676896792/',
+    twitter: '@joeyhipolito_',
+    www: 'http://www.joeyhipolito.com',
+    github: 'http://github.com/joeyhipolito'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
